516059670645c6155bb7ecdb8c8efc1a8fae98fd
Alvaro Aspee <aspeealvaro@gmail.com>
2020-06-20 01:48:24 -0400

    Juego x maquina

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import TicTacToe from './TicTacToe.vue';
+import Cell from './Cell.vue';
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+Vue.component('tic-tac-toe', TicTacToe);
+Vue.component('cell', Cell);
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+});
